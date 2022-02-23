02a82c176f841ee6bac77cbb58ff11a1634409dc
Gerard Compion <gcompion93@gmail.com>
2022-02-23 18:32:45 -0500

    add story for Appointments with a booking and update props used by Show component when rendered by Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,16 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
 export default function Appointment(props) {
+  const interview = props.interview;
+
   return (
       <article className="appointment">
         <Header time={props.time} />
-        {props.interview ? <Show /> : <Empty />}
+        {props.interview ? <Show interviewer={interview.interviewer} student={interview.student} /> : <Empty />}
       </article>
   );
 }
