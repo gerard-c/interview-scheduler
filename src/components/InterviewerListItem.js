c9819bbf44ec439ac779c831243b81d0790a9285
Gerard Compion <gcompion93@gmail.com>
2022-02-23 09:20:25 -0500

    refactor InterviewerListItem to use props/conditional classnames/rendering

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
-import 'components/InterviewerListItem.scss'
+import React from 'react';
+import classNames from 'classnames';
+import 'components/InterviewerListItem.scss';
 
-export default function InterviewerListItem() {
+export default function InterviewerListItem(props) {
+  // const [interviewer, setInterviewer] = useState(0);
+  const interviewerClass = classNames('interviewers__item', {
+    'interviewers__item--selected': props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && <p>{props.name}</p>}
     </li>
   );
 }
