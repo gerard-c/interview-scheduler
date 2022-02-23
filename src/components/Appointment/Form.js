a0fbcf566c441118b9038068f323aa9663658287
Gerard Compion <gcompion93@gmail.com>
2022-02-23 17:59:55 -0500

    fixed bug caused by cancel returning props.onCancel definition instead of calling props.onCancel() itself

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -12,9 +12,13 @@ export default function From(props) {
   }
 
   const cancel = () => {
+    console.log(props);
     reset();
-    return props.onCancel;
+    return props.onCancel();
   }
+
+  console.log(cancel);
+  console.log(props.onCancel);
 
   return (
     <main className="appointment__card appointment__card--create">
