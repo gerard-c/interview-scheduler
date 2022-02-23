366f71439df4d124030fb3fc14f321c1fea9fa55
Gerard Compion <gcompion93@gmail.com>
2022-02-23 08:39:14 -0500

    add stories, styling for InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import 'components/InterviewerListItem.scss'
 
 export default function InterviewerListItem() {
   return (
