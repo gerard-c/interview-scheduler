d94a4b284403defa3f01ca4652940d0e9b1420ff
Gerard Compion <gcompion93@gmail.com>
2022-02-24 16:10:57 -0500

    confirm that selector passes all tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,18 +1,23 @@
-export default function getAppointmentsForDay(state, day) {
-  let targetDay = null;
-  const appointmentsArray = Object.values(state.appointments);
+export function getAppointmentsForDay(state, day) {
   const resultsArray = [];
+  let targetDay = null;
 
+  if (!state.days.length) {
+    return resultsArray;
+  }
+
+  const appointmentsArray = Object.values(state.appointments);
+  
   for (const item of state.days) {
     if (item.name === day) {
       targetDay = item;
     }
   }
 
-  if (targetDay.appointments.length === 0) {
+  if (!targetDay) {
     return resultsArray;
   }
-
+  
   for (const appointmentId of targetDay.appointments) {
     for (const appointment of appointmentsArray) {
       if (appointmentId === appointment.id) {
