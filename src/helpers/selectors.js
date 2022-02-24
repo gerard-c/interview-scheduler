751f4072f98d2663aad6e2c0bde1007d2613c7a6
Gerard Compion <gcompion93@gmail.com>
2022-02-24 15:54:33 -0500

    add logic to getAppointmentsForDay selector to return array of appointment objects

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,24 @@
-const getAppointmentsForDay = (state, name) => {
-  
+export default function getAppointmentsForDay(state, day) {
+  let targetDay = null;
+  const appointmentsArray = Object.values(state.appointments);
+  const resultsArray = [];
+
+  for (const item of state.days) {
+    if (item.name === day) {
+      targetDay = item;
+    }
+  }
+
+  if (targetDay.appointments.length === 0) {
+    return resultsArray;
+  }
+
+  for (const appointmentId of targetDay.appointments) {
+    for (const appointment of appointmentsArray) {
+      if (appointmentId === appointment.id) {
+        resultsArray.push(appointment);
+      }
+    }
+  }
+  return resultsArray;
 }
