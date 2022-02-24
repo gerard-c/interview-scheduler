0e6fe3efa007320d5983b8a1df0c83b9d876c2b0
Gerard Compion <gcompion93@gmail.com>
2022-02-23 19:23:01 -0500

    setup Application component to render Appointments in a schedule using hard-coded dummy data

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+
+import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
   const interview = props.interview;
