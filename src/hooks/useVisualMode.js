16cb6979e8ef6a2670bda2ce2f38c45bbdda37cf
Gerard Compion <gcompion93@gmail.com>
2022-02-25 17:35:59 -0500

    implement history state, update transition to add newMode to end of history array, implement functionality for back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,19 @@ import { useState } from "react";
 
 export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   const transition = (newMode) => {
     setMode(newMode);
+    setHistory(prev => [...prev, newMode]);
   }
 
-  return { mode, transition };
+  const back = () => {
+    history.pop();
+    setMode(history[history.length - 1]);
+  }
+
+  console.log(history);
+  console.log(mode);
+  return { mode, transition, back };
 }
