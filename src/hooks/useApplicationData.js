b01f6ce76489437f811b501e457f4e0115b87f26
Gerard Compion <gcompion93@gmail.com>
2022-02-28 22:16:27 -0500

    implement auto updating 'spots' counters for days on nav

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { getAppointmentsForDay } from "helpers/selectors";
 import axios from "axios";
 
 export function useApplicationData() {
@@ -8,47 +9,74 @@ export function useApplicationData() {
     appointments: {},
     interviewers: {}
   });
-  
+
   const setDay = day => setState({ ...state, day });
-  
+
   const bookInterview = (id, interview) => {
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
     };
-  
+
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
-  
-    setState({
-      ...state,
-      appointments
-    });
-  
-    return axios.put(`/api/appointments/${id}`, appointment);
+
+    const days = [...state.days]
+    
+    const index = days.map(day => day.name).indexOf(state.day);
+
+    const day = days[index]
+    
+    if (!state.appointments[id].interview) {
+      day.spots--;
+    }
+
+    days[index] = day;
+
+    return axios
+      .put(`/api/appointments/${id}`, appointment)
+      .then(() => {
+        setState({
+          ...state,
+          appointments,
+          days
+        });
+      })
   }
-  
+
   const cancelInterview = (id) => {
     const appointment = {
       ...state.appointments[id],
       interview: null
     };
-  
+
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
-  
-    setState({
-      ...state,
-      appointments
-    });
-  
-    return axios.delete(`/api/appointments/${id}`);
+
+    const days = [...state.days];
+
+    const index = days.map(day => day.name).indexOf(state.day);
+    console.log(state.day.name);
+    
+    const day = days[index];
+    
+    day.spots++;
+    
+    days[index] = day;
+
+    return axios
+      .delete(`/api/appointments/${id}`)
+      .then(() => setState({
+        ...state,
+        appointments,
+        days
+      }));
   }
-  
+
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
@@ -58,7 +86,7 @@ export function useApplicationData() {
       const daysData = all[0].data;
       const appointmentsData = all[1].data;
       const interviewersData = all[2].data;
-  
+
       setState(prev => ({
         ...prev,
         days: daysData,
