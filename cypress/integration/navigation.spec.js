661303482160b6ef4e9535bc3ca16dafd7283e3c
Gerard Compion <gcompion93@gmail.com>
2022-03-02 15:33:34 -0500

    refactor Tuesday test to find the list element, click it and check the color in one go

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -7,11 +7,8 @@ describe('Navigation', () => {
     cy.visit('/');
 
     cy.get('li')
-      .contains('Tuesday')
-      .click();
-
-    cy.get('li')
       .contains('li', 'Tuesday')
+      .click()
       .should('have.css', 'background-color', 'rgb(242, 242, 242)');
   });
 });
